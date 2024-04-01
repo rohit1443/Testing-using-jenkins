00baee6ca4720dceafbf07547d2e0b5e9b99f0e0
Rohit Kumar <126372210+rohit1443@users.noreply.github.com>
2024-04-01 23:10:24 +0530

    Update testsite.py

--- testsite.py
+++ testsite.py
@@ -7,7 +7,7 @@ class TestWebsiteLoading(unittest.TestCase):
 
     def test_website_loading(self):
         print("Testing if atgm.world website loads...")
-        response = requests.get(website_url)
+        response = requests.get(https://atg.world/)
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
