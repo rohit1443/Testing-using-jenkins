4876e784ca84107781fa030918697d4932b3c2e3
Rohit Kumar <126372210+rohit1443@users.noreply.github.com>
2024-04-01 23:05:30 +0530

    Update testsite.py

--- testsite.py
+++ testsite.py
@@ -1,20 +1,14 @@
-import unittest
 import requests
+import unittest
+
+website_url = "https://atgm.world"  # Corrected the domain name
 
 class TestWebsiteLoading(unittest.TestCase):
-    "Class to unit check the loading of atg.world website"
 
     def test_website_loading(self):
-        "Unit test to check if the atgw.wworld website loads properly"
-        
-        print("Testing if atg.world website loads...")
-
-        website_url = "https://atgm.worldm"
+        print("Testing if atgm.world website loads...")
         response = requests.get(website_url)
-        
-        self.assertEqual(response.status_code, 200, f"Failed to load website: {website_url}")
-
-        print("Website loading test completed.")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
